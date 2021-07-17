1c6e2dcc2defc4fbd27cf5ce7e7b9cb590f605fd
samyeak-m <60485596+samyeak-m@users.noreply.github.com>
2021-07-17 22:02:22 +0545

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Distance {
+public:
+	int feet, inch;
+	
+	Distance()
+	{
+		this->feet = 0;
+		this->inch = 0;
+	}
+	
+	Distance(int f, int i)
+	{
+		this->feet = f;
+		this->inch = i;
+	}
+
+	Distance operator-(Distance& d2)
+	{
+		Distance d3;
+		d3.feet = this->feet - d2.feet;
+		d3.inch = this->inch - d2.inch;
+		
+		return d3;
+	}
+};
+
+int main()
+{
+	Distance d1(9, 5);
+
+	Distance d2(3, 1);
+
+	Distance d3;
+
+	d3 = d1 - d2;
+
+	cout << "\nTotal Feet & Inches: " << d3.feet << "'" << d3.inch;
+	return 0;
+}
+
